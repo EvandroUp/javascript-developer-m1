5c448b6044c0bc02b908bb8937a83495c395d16c
Evandroups <151587304+EvandroUp@users.noreply.github.com>
2024-01-31 16:38:21 -0300

    exercicio3.js

--- 08 - avaliacao/exercicio3.js
+++ 08 - avaliacao/exercicio3.js
@@ -20,3 +20,32 @@
 //             2050.00
 
 const { gets, print } = require('./funcoes-auxiliares-ex3');
+
+const valorSalarioBruto = gets();
+const valorBeneficios = gets();
+
+
+function calcularPorcentagem(valor, percentual) {
+    return valor * (percentual / 100);
+
+}
+
+function pegarAliquota(salario) {
+    if (salario >= 0 && salario <= 1000) {
+        return 5;
+    }
+    else if (salario >= 1100.01 && salario <= 2500) {
+        return 10;
+    }
+    else {
+        return 15;
+    }
+
+
+
+}
+const aliquotaDoImposto = pegarAliquota(valorSalarioBruto);
+const valorImposto = calcularPorcentagem(valorSalarioBruto,aliquotaDoImposto);
+const valorAtransferir = valorSalarioBruto - valorImposto + valorBeneficios;
+
+print(valorAtransferir);
